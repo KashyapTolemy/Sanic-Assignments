6352d295c0909a250e3e4635115fd3ec77ec5e18
Tolemy <tolemy@FVFJD07KQ6L4.local>
2025-01-05 23:53:59 +0530

    db not working

--- task_manager/server.py
+++ task_manager/server.py
@@ -1,68 +1,91 @@
 from sanic import Sanic, Request, response
 from utils.models import Task
+from tortoise import Tortoise, run_async
 
 app = Sanic(__name__)
 tasks = []
 task_counter = 1
 
+
+TORTOISE_CONNECT = {
+    "connections": {
+        "default": "mysql://root:uknowwhoiam@localhost:3306/task_manager"
+    },
+    "apps": {
+        "models": {
+            "models": ["utils.models"],
+            "default_connection": "default",
+        }
+    }
+}
+
+
+
+async def connect_to_db():
+    await Tortoise.init(
+        db_url='mysql://root:uknowwhoiam@localhost:3306/task_manager',
+        modules={"models": ["utils.models"]}, 
+    )    
+    await Tortoise.generate_schemas()
+
+
+
 @app.post("/tasks")
 async def create_task(request: Request):
-    global task_counter
-    task= {
-        "id": task_counter,
-        "title": request.json.get("title"),
-        "description": request.json.get("description"),
-        "status": request.json.get("status","pending")
-    }
-    task_counter += 1
-    tasks.append(task)
-    return response.json(f"Task {task['id']} : {task['title']} Created.")
+    task = await Task.create(
+        title = request.json.get('title'),
+        description = request.json.get('description'),
+        status = request.json.get('status','pending')
+        )
+    return response.json(f"Task {task.id} : {task.title} Created.")
 
 
 @app.get("/tasks")
 async def get_all_tasks(request: Request):
+    tasks = await Task.all()
     return response.json(f"{tasks}\n\n All Tasks received.")
 
 
-@app.get("/tasks/<id>")
-async def get_task(request: Request,id):
-    task = None
-    for t in tasks:
-        if str(t['id']) == str(id):
-            task = t
-            break
-    if task is None:
-        return response.json(f"Task {id} not found.",status = 404)
-    return response.json(task)
+# @app.get("/tasks/<id>")
+# async def get_task(request: Request,id):
+#     task = None
+#     for t in tasks:
+#         if str(t['id']) == str(id):
+#             task = t
+#             break
+#     if task is None:
+#         return response.json(f"Task {id} not found.",status = 404)
+#     return response.json(task)
 
 
-@app.put("tasks/<id>")
-async def update_task(request: Request,id):
-    task = None
-    for t in tasks:
-        if str(t['id']) == str(id):
-            task = t
-            break
-    if task is None:
-        return response.json(f"Task {id} not found.",status = 404)
-    task['title'] = request.json.get('title',task['title'])
-    task['description'] = request.json.get('description',task['description'])
-    task['status'] = request.json.get('status',task['status'])
-    return response.json(f"Task {id} updated.\n{tasks}")
+# @app.put("tasks/<id>")
+# async def update_task(request: Request,id):
+#     task = None
+#     for t in tasks:
+#         if str(t['id']) == str(id):
+#             task = t
+#             break
+#     if task is None:
+#         return response.json(f"Task {id} not found.",status = 404)
+#     task['title'] = request.json.get('title',task['title'])
+#     task['description'] = request.json.get('description',task['description'])
+#     task['status'] = request.json.get('status',task['status'])
+#     return response.json(f"Task {id} updated.\n{tasks}")
 
 
-@app.delete("tasks/<id>")
-async def delete_task(request: Request,id):
-    task = None
-    for t in tasks:
-        if str(t['id']) == str(id):
-            task = t
-            break
-    if task is None:
-        return response.json(f"Task {id} not found.",status = 404)
-    tasks.remove(task)
-    return response.json(f"Task {id} deleted.")
+# @app.delete("tasks/<id>")
+# async def delete_task(request: Request,id):
+#     task = None
+#     for t in tasks:
+#         if str(t['id']) == str(id):
+#             task = t
+#             break
+#     if task is None:
+#         return response.json(f"Task {id} not found.",status = 404)
+#     tasks.remove(task)
+#     return response.json(f"Task {id} deleted.")
 
 
 if __name__ == '__main__':
+    run_async(connect_to_db())
     app.run(port = 9999, debug = True)
--- task_manager/utils/models.py
+++ task_manager/utils/models.py
@@ -1,9 +1,9 @@
 from tortoise import fields, models
 
-class Task(models.Models):
+class Task(models.Model):
     id = fields.IntField(pk=True)
     title = fields.CharField(max_length = 50)
-    description = fields.CharField(maxlentgh = 200,null = True)
+    description = fields.CharField(max_length = 200,null = True)
     status = fields.CharField(max_length = 20,default = "pending")
 
 def __str__(self):
